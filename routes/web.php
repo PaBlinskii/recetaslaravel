078a8279283c5c468f6e0a6e9ac71915d33e8b11
PantoniDev <ipxb10@gmail.com>
2022-02-16 21:17:03 -0600

    Configurando Laravel 7

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/nosotros', 'RecetaController'); // Usando __invoke
+// Route::get('/nosotros', 'RecetasController@hola');
+// Route::get('/nosotros',[RecetaController::class, 'hola']); // En Laravel 8
